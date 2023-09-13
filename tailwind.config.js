c0b5c8b3310646dfd0b1156f01d5a593d07a1bfc
MooseCowBear <82609260+MooseCowBear@users.noreply.github.com>
2023-09-13 18:39:53 -0400

    update tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,18 @@
 module.exports = {
   content: [
-    './app/views/**/*.html.erb',
-    './app/helpers/**/*.rb',
-    './app/assets/stylesheets/**/*.css',
-    './app/javascript/**/*.js'
-  ]
-}
+    "./app/views/**/*.html.erb",
+    "./app/helpers/**/*.rb",
+    "./app/assets/stylesheets/**/*.css",
+    "./app/javascript/**/*.js",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Inter var", ...defaultTheme.fontFamily.sans],
+      },
+      gridTemplateColumns: {
+        autofit: "repeat(auto-fit, minmax(280px, 1fr))",
+      },
+    },
+  },
+};
