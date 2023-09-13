167a9606252f2896f5f34fd19c6afe2c14cac413
MooseCowBear <82609260+MooseCowBear@users.noreply.github.com>
2023-09-13 18:10:03 -0400

    add public rooms controller

---
+++ app/controllers/api/v1/public_rooms_controller.rb
@@ -0,0 +1,44 @@
+class Api::V1::PublicRoomsController < ApplicationController
+  def index
+    rooms = Room.public_rooms
+    render json: rooms
+  end
+
+  def create
+    room = Room.new(room_params)
+    room.creator = current_user
+    if room.save 
+      render json: room
+    else
+      render json: { message: "Validations Failed", 
+                    errors: room.errors.full_messages }, 
+                    status: :unprocessable_entity
+    end
+  end
+
+  def show
+    room = Room.find(params[:id])
+
+    unless room.participant?(current_user)
+      render json: { message: "Unauthorized", 
+                    errors: ["This private chat does not belong to you"] },
+                    status: 401
+      
+    else
+      messages = room.messages.order(created_at: :asc)
+      render json: messages, include: [:user]
+    end
+  end
+
+  def destroy
+  end
+
+  def update
+  end
+
+  private 
+
+  def room_params 
+    params.require(:room).permit(:name, :is_private)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
+      get 'public_rooms/index'
+      post 'public_rooms/create'
+      get 'public_rooms/show/:id', to: "public_rooms#show"
+      delete 'public_rooms/destroy', to: "public_rooms#destory"
+      post 'public_rooms/update', to: "public_rooms#update"
       post 'messages/create'
       post 'messages/update/:id', to: "messages#update"
       delete 'messages/destroy', to: 'messages#destroy'
