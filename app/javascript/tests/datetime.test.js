da3c5d06eed0376ce6d20fde50e3215d63269df7
MooseCowBear <82609260+MooseCowBear@users.noreply.github.com>
2023-09-20 14:40:42 -0400

    add welcome component test

--- app/javascript/tests/datetime.test.js
+++ app/javascript/tests/datetime.test.js
@@ -1,11 +1,11 @@
 import { displayDateTime } from "../helpers/datetime";
 
 describe("displayDateTime", () => {
-  test("displays the date if not today", () => {
+  it("displays the date if not today", () => {
     expect(displayDateTime("2010-05-30T06:14:00Z")).toBe("5/30/2010");
   });
 
-  test("displays time if today", () => {
+  it("displays time if today", () => {
     let today = new Date();
     today.setHours(0, 0, 0, 0);
     expect(displayDateTime(today)).toBe("12:00 am");
--- app/javascript/tests/users.test.js
+++ app/javascript/tests/users.test.js
@@ -1,11 +1,11 @@
 import { isUserActive } from "../helpers/users";
 
 describe("isUserActive", () => {
-  test("it returns true when user's last active datetime is is less than 10 minutes ago", () => {
+  it("it returns true when user's last active datetime is is less than 10 minutes ago", () => {
     expect(isUserActive(new Date())).toBeTruthy();
   });
 
-  test("it returns false when user's last active datetime is greater than 10 minutes ago", () => {
+  it("it returns false when user's last active datetime is greater than 10 minutes ago", () => {
     const now = new Date();
     const testTime = new Date(now.getTime() - (10 * 60000 + 1));
     expect(isUserActive(testTime)).toBeFalsy();
