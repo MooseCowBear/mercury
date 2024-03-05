a6956dddff59a156c811533774f3394b1187320a
MooseCowBear <82609260+MooseCowBear@users.noreply.github.com>
2024-03-05 15:55:05 -0500

    update factory

--- spec/factories/chats.rb
+++ spec/factories/chats.rb
@@ -1,19 +1,26 @@
 FactoryBot.define do
   factory :chat do
     name { "test chat" }
-    is_private { false }
+
+    trait :public do
+      is_private { false }
+    end
+
+    trait :private do
+      is_private { true }
+    end
 
     trait :over_week_old do
-      name { "old room" }
+      name { "old chat" }
       updated_at { 10.days.ago }
     end
 
     trait :within_week_old do
-      name { "newish room" }
+      name { "newish chat" }
       updated_at { 1.day.ago }
     end
 
-    factory :old_room, traits: [:over_week_old]
-    factory :new_room, traits: [:within_week_old]
+    factory :old_chat, traits: [:over_week_old]
+    factory :new_chat, traits: [:within_week_old]
   end
 end
