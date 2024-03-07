11a8f175a91c7ea1db39fd477dbe7e46558351de
MooseCowBear <82609260+MooseCowBear@users.noreply.github.com>
2024-03-07 08:40:57 -0500

    refactor

--- app/services/private_chat_create_service.rb
+++ app/services/private_chat_create_service.rb
@@ -1,4 +1,8 @@
-class PrivateChatCreateService 
+class PrivateChatCreateService < ApplicationService
+  # want private chats to be unique to set of participants
+  # so if a chat already exists with participants, want to find it
+  # and if doesn't exist, then want to create both it and the participants that 
+  # belong to/define it
   attr_accessor :params, :current_user
 
   def initialize(params, current_user)
@@ -6,13 +10,13 @@ class PrivateChatCreateService
     @current_user = current_user
   end
 
-  def self.call(*args)
-    new(*args).find_or_create
+  def call
+    find_or_create
   end
 
+  private
+
   def find_or_create
-    # returns the existing chat with those particpants 
-    # or creates a new chat and the chat participants and returns it
     user_ids = params.dig(:chat_participants_attributes).map { |key| key[:user_id] }
     return unless user_ids.include?(current_user.id)
 
