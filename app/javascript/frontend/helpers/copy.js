231735c03f15eaa79fd3acb006c11c68a322ddd9
MooseCowBear <82609260+MooseCowBear@users.noreply.github.com>
2024-03-08 16:45:03 -0500

    rename folder

--- app/javascript/frontend/helpers/copy.js
+++
@@ -1,6 +0,0 @@
-export default copyObjectArr = (arr) => {
-  const copy = arr.map((a) => {
-    return { ...a };
-  });
-  return copy;
-}
--- app/javascript/frontend/helpers/users.js
+++
@@ -1,7 +0,0 @@
-export const isUserActive = (lastActiveDateString) => {
-  if (!lastActiveDateString) return false;
-  const lastActive = new Date(lastActiveDateString);
-  const now = new Date();
-  const diffInMinutes = (now.getTime() - lastActive.getTime()) / 60000;
-  return diffInMinutes <= 10;
-}
