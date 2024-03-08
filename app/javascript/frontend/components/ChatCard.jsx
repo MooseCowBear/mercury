55d72daad99bdb2ded7ac8432e5c053ef7ae4036
MooseCowBear <82609260+MooseCowBear@users.noreply.github.com>
2024-03-08 16:32:11 -0500

    update chat card

--- app/javascript/frontend/components/ChatCard.jsx
+++ app/javascript/frontend/components/ChatCard.jsx
@@ -1,26 +1,47 @@
 import React from "react";
+import { displayDateTime } from "../utils/datetime";
 
-export default function ChatCard({ chat, isPrivate }) {
-  // won't need isPrivate, just for working out ui
+export default function ChatCard({ chat }) {
+  const isPrivate = chat.is_private;
+
+  const privateTitle = (chat) => {
+    return chat.users.map((u) => u.username).join(", ");
+  };
+
+  const title = isPrivate ? privateTitle(chat) : chat.name;
+
+  const time = chat.last_message
+    ? displayDateTime(chat.last_message.created_at)
+    : "";
+
+  const preview = chat.last_message
+    ? chat.last_message.body
+      ? chat.last_message.body
+      : ""
+    : "";
+
   return (
     <button className="w-full flex justify-between items-center py-2">
       <div className="grid grid-col-[auto,_1fr] grid-rows-2 gap-x-2 items-center">
         <div className="size-10 rounded-full row-span-2 bg-neutral-800"></div>
-        <h4 className="text-sm font-medium col-start-2 text-left">
-          name placeholder
+        <h4 className="text-sm font-medium col-start-2 text-left text-ellipsis overflow-hidden">
+          {title}
         </h4>
-        <p className="text-xs text-neutral-500 col-start-2 text-left">
-          message preview placeholder...
+        <p className="text-xs text-neutral-500 col-start-2 text-left text-ellipsis overflow-hidden">
+          {preview}
         </p>
       </div>
-      {isPrivate && (
-        <div className="flex flex-col gap-2 items-center">
-          <p className="text-xs">10:35</p>
-          <div className="flex items-center justify-center rounded-full size-6 bg-poppy-500 text-white text-sm">
-            3
-          </div>
+
+      <div className="flex flex-col gap-2 items-end justify-between">
+        <p className="text-xs self-start">{time}</p>
+        <div
+          className={`flex items-center justify-center rounded-full size-6 text-white text-sm ${
+            isPrivate && "bg-poppy-500"
+          }`}
+        >
+          3
         </div>
-      )}
+      </div>
     </button>
   );
 }
--- app/javascript/frontend/components/ChatsContainer.jsx
+++ app/javascript/frontend/components/ChatsContainer.jsx
@@ -1,14 +1,74 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ChatCard from "./ChatCard";
 
-export default function ChatsContainer({ title, chats, isPrivate }) {
+export default function ChatsContainer({ title, isPrivate }) {
+  const [chats, setChats] = useState([]);
+
+  useEffect(() => {
+    const abortController = new AbortController();
+
+    const getPrivateChats = async () => {
+      try {
+        const response = await fetch("/api/v1/private_chats/index", {
+          mode: "cors",
+          signal: abortController.signal,
+        });
+
+        if (response.status >= 400) {
+          console.log(response.status);
+          throw new Error("server error");
+        }
+        const data = await response.json();
+        setChats(data);
+      } catch (e) {
+        console.log(e);
+      }
+    };
+
+    const getPublicChats = async () => {
+      try {
+        const response = await fetch("/api/v1/public_chats/index", {
+          mode: "cors",
+          signal: abortController.signal,
+        });
+
+        if (response.status >= 400) {
+          console.log(response.status);
+          throw new Error("server error");
+        }
+        const data = await response.json();
+        setChats(data);
+      } catch (e) {
+        console.log(e);
+      }
+    };
+    if (isPrivate) {
+      getPrivateChats();
+    } else {
+      getPublicChats();
+    }
+
+    return () => {
+      abortController.abort(); // cancel request if unmount
+    };
+  }, []);
+
   return (
-    <div className="py-4 px-2">
+    <div className="py-4 px-2 min-h-0 min-w-0">
       <h3 className="text-xs uppercase text-neutral-500">{title}</h3>
       <div className="flex flex-col divide-y-[1px]">
-        <ChatCard isPrivate={isPrivate} />
-        <ChatCard isPrivate={isPrivate} />
+        {chats.map((chat) => {
+          return <ChatCard key={chat.id} chat={chat} />;
+        })}
       </div>
     </div>
   );
 }
+
+// this is where we will want to consume the action cable broadcast for chats
+// if chats is a state of this container, is that ok?
+
+// current chat should be a context to be consumed here
+// and also in the messages container...
+
+// which channel it listens to depends on whether its private or not
