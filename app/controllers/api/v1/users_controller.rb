fe824f5d92130e4fa8daa549721f465ec7ec4433
MooseCowBear <82609260+MooseCowBear@users.noreply.github.com>
2024-03-09 09:53:32 -0500

    add action to update curr chat

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,5 +1,6 @@
 class Api::V1::UsersController < ApplicationController
   after_action -> { current_user.update_last_active if current_user }
+  before_action :set_chat, only: [:update]
   
   def show
     render json: current_user
@@ -8,5 +9,25 @@ class Api::V1::UsersController < ApplicationController
   def index
     users = User.other_users(current_user).ordered_by_username
     render json: users
+  end
+
+  def update
+    if @chat.participant?(current_user)
+      current_user.update(user_params) 
+      render json: current_user
+    else
+      render json: { message: "Bad Request" }, 
+                    status: :unprocessable_entity
+    end
+  end
+
+  private 
+
+  def user_params
+    params.require(:user).permit(:current_chat_id)
+  end
+
+  def set_chat
+    @chat = Chat.find(user_params[:current_chat_id])
   end
 end
