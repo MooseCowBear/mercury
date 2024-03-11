ba45abdea2792f9f0a7803a01a5723cf8a850b1e
MooseCowBear <82609260+MooseCowBear@users.noreply.github.com>
2024-03-11 08:22:15 -0400

    add send message button

---
+++ app/javascript/frontend/components/SendMessageButton.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function SendMessageButton({ submitHandler }) {
+  return (
+    <button aria-label="send" onClick={submitHandler}>
+      <svg
+        className="size-6 fill-poppy-500"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 24 24"
+      >
+        <path fill="" d="M2,21L23,12L2,3V10L17,12L2,14V21Z" />
+      </svg>
+    </button>
+  );
+}
