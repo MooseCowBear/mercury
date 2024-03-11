3913d7c6f806d245f39334fb28f33b0e617e3388
MooseCowBear <82609260+MooseCowBear@users.noreply.github.com>
2024-03-11 18:16:17 -0400

    remove broadcast'

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -5,12 +5,4 @@ class Notification < ApplicationRecord
 
   scope :for_user, ->(user) { where(user: user) }
   scope :for_chat, ->(chat) { where(chat: chat) }
-
-  after_commit :broadcast_notification, on: [:create, :destroy]
-
-  private 
-
-  def broadcast_notification
-    ActionCable.server.broadcast("notification_#{self.user_id}", self)
-  end
 end
