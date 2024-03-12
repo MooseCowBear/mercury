e1f90714152ab81a834a91da922b7dcc0f0c220e
MooseCowBear <82609260+MooseCowBear@users.noreply.github.com>
2024-03-12 17:58:22 -0400

    add button to form for new public chat

--- app/javascript/frontend/components/ChatsContainer.jsx
+++ app/javascript/frontend/components/ChatsContainer.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState, useRef } from "react";
 import ChatCard from "./ChatCard";
+import Plus from "../icons/Plus";
 import { getResource } from "../utils/apiRequest";
 import { useActionCableContext } from "../contexts/ActionCableContext";
 import {
@@ -11,11 +12,14 @@ import {
   subscribeToPublicChatsChannel,
   unsubscribeToPublicChatsChannel,
 } from "../../channels/public_chats_channel";
+import NewPublicChatForm from "./NewPublicChatForm";
 
 export default function ChatsContainer({ title, isPrivate }) {
   const { userInfo } = useUserInfoContext();
   const { cable } = useActionCableContext();
   const [chats, setChats] = useState([]);
+
+  const [newChatForm, setNewChatForm] = useState(false);
 
   const chatChannelRef = useRef(null);
 
@@ -69,10 +73,29 @@ export default function ChatsContainer({ title, isPrivate }) {
     };
   }, [userInfo]); // does this need to depend on userinfo?
 
+  const clickHandler = () => {
+    if (isPrivate) {
+      // show people -- should move visibility to a context since this will need it
+    } else {
+      setNewChatForm(true);
+    }
+  };
+
   return (
     <div className="py-4 min-h-0 min-w-0">
-      <h3 className="px-2 text-xs uppercase text-neutral-500">{title}</h3>
+      <div className="px-2 mb-1 flex justify-between items-center">
+        <h3 className="text-xs uppercase text-neutral-500 leading-none">
+          {title}
+        </h3>
+        <button
+          onClick={clickHandler}
+          className="bg-poppy-500 p-1 rounded-full"
+        >
+          <Plus />
+        </button>
+      </div>
       <div className="flex flex-col divide-y-[1px]">
+        {newChatForm && <NewPublicChatForm setNewChatForm={setNewChatForm} />}
         {chats.map((chat) => {
           return <ChatCard key={chat.id} chat={chat} />;
         })}
