a434595933c067f2fb00d9ec226c38e1df00c35b
MooseCowBear <82609260+MooseCowBear@users.noreply.github.com>
2024-03-13 08:05:42 -0400

    add new public chat request

--- app/javascript/frontend/components/NewPublicChatForm.jsx
+++ app/javascript/frontend/components/NewPublicChatForm.jsx
@@ -1,6 +1,11 @@
 import React, { useState } from "react";
+import { postResource } from "../utils/apiRequest";
+import { useUserInfoContext } from "../contexts/UserInfoContext";
+import { useVisibilityContext } from "../contexts/VisibilityContext";
 
 export default function NewPublicChatForm({ setNewChatForm }) {
+  const { setUserInfo } = useUserInfoContext();
+  const { chatVisibilityHandler } = useVisibilityContext();
   const [input, setInput] = useState("");
   const [error, setError] = useState(null);
 
@@ -11,19 +16,44 @@ export default function NewPublicChatForm({ setNewChatForm }) {
   const submitHandler = (e) => {
     e.preventDefault();
 
-    if (input.trim() === "") {
-      setError("Chat must have a name.");
+    if (input.trim() === "" || input.trim().length > 45) {
+      setError("Chat must have a name under 45 characters.");
       return;
     }
 
-    // create new public chat AND update curr user's room to it
+    const dataHandler = (data) => {
+      if (data.hasOwnProperty("errors")) {
+        console.log(data);
+        setError(data.errors.join(", "));
+      } else {
+        setNewChatForm(false);
+        setUserInfo(data);
+        // on desktop, won't do anything. on mobile will swap the chats for messages
+        // bc if you created a room, then you probably will want to send a message to it
+        chatVisibilityHandler();
+      }
+    };
+
+    postResource(
+      "/api/v1/public_chats/create",
+      JSON.stringify({ name: input }),
+      "POST",
+      dataHandler
+    );
   };
+
   const cancelHandler = () => {
     setNewChatForm(false);
   };
+
   return (
-    <form className="w-full flex flex-col gap-1 justify-between items-center py-2 px-3">
+    <form
+      onSubmit={submitHandler}
+      className="w-full flex flex-col gap-1 justify-between items-center py-2 px-3"
+    >
+      {error && <span className="justify-self-center text-xs">{error}</span>}
       <input
+        aria-label="new chat name"
         type="text"
         onChange={changeHandler}
         className="w-full border rounded-full px-3 py-1 text-sm lowercase tracking-wider"
@@ -36,7 +66,7 @@ export default function NewPublicChatForm({ setNewChatForm }) {
         >
           cancel
         </button>
-        <button onClick={submitHandler} className="text-xs uppercase">
+        <button type="submit" className="text-xs uppercase">
           create
         </button>
       </div>
