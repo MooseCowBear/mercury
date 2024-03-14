0b094a7d2e1e3c6461047506478ca709039e024b
MooseCowBear <82609260+MooseCowBear@users.noreply.github.com>
2024-03-14 09:26:02 -0400

    update comment

--- app/services/private_chat/create_service.rb
+++ app/services/private_chat/create_service.rb
@@ -1,5 +1,6 @@
 class PrivateChat::CreateService < ApplicationService
-  # want private chats to be unique to set of participants
+  # want private chats to be unique to set of participants determined at the time
+  # of creation, regardless of whether one or more users have deleted their accounts since
   # so if a chat already exists with participants, want to find it
   # and if doesn't exist, then want to create both it and the participants that 
   # belong to/define it
@@ -30,4 +31,3 @@ class PrivateChat::CreateService < ApplicationService
     chat
   end
 end
-
