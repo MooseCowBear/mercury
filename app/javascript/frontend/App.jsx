9719fc241ae1ca982babd049d2463eea92cf56c3
MooseCowBear <82609260+MooseCowBear@users.noreply.github.com>
2024-03-14 17:36:12 -0400

    move context providers to app so can use contexts in dashboard

--- app/javascript/frontend/App.jsx
+++ app/javascript/frontend/App.jsx
@@ -1,6 +1,22 @@
 import React from "react";
 import Router from "../routes/index.jsx";
+import { ActionCableProvider } from "./contexts/ActionCableContext.jsx";
+import { UserInfoProvider } from "./contexts/UserInfoContext.jsx";
+import { PrivateChatsProvider } from "./contexts/PrivateChatsContext.jsx";
+import { PublicChatsProvider } from "./contexts/PublicChatsContext.jsx";
 
-export default App = (props) => {
-  return <Router />;
+export default App = () => {
+  return (
+    <ActionCableProvider>
+      <UserInfoProvider>
+        <PrivateChatsProvider>
+          <PublicChatsProvider>
+            <Router />
+          </PublicChatsProvider>
+        </PrivateChatsProvider>
+      </UserInfoProvider>
+    </ActionCableProvider>
+  );
 };
+
+// TRY: moving action cable provider here so dashboard has access
