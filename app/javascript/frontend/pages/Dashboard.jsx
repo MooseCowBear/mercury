ab189b4587b01d1d4e7bda1a5cca8ffadd2aa5d1
MooseCowBear <82609260+MooseCowBear@users.noreply.github.com>
2024-03-14 17:37:36 -0400

    update dashboard to do the initial fetch for chats and hold the cable subscriptions

--- app/javascript/frontend/pages/Dashboard.jsx
+++ app/javascript/frontend/pages/Dashboard.jsx
@@ -1,25 +1,88 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import ChatMain from "../layout/ChatMain.jsx";
 import Menu from "../layout/Menu.jsx";
 import ChatSidebar from "../layout/ChatSidebar.jsx";
 import PeopleSidebar from "../layout/PeopleSidebar.jsx";
-import { UserInfoProvider } from "../contexts/UserInfoContext.jsx";
-import { ActionCableProvider } from "../contexts/ActionCableContext.jsx";
 import { VisibilityProvider } from "../contexts/VisibilityContext.jsx";
+import { useActionCableContext } from "../contexts/ActionCableContext.jsx";
+import { useUserInfoContext } from "../contexts/UserInfoContext.jsx";
+import { usePrivateChatsContext } from "../contexts/PrivateChatsContext.jsx";
+import { usePublicChatsContext } from "../contexts/PublicChatsContext.jsx";
+import { updateChats } from "../utils/chats.js";
+import {
+  subscribeToPrivateChatsChannel,
+  unsubscribeToPrivateChatsChannel,
+} from "../../channels/private_chats_channel.js";
+import {
+  subscribeToPublicChatsChannel,
+  unsubscribeToPublicChatsChannel,
+} from "../../channels/public_chats_channel.js";
+
+/* dashboard subscribes to the private, public chat channels. these are channels
+that listen for new chats being created/existing chats being updated. 
+in order to have a chat appear to creator when chat is created, but
+not appearing to others until a message is created, need to update chats on front end for 
+creator from chat#create json response
+*/
 
 export default function Dashboard() {
+  const { cable } = useActionCableContext();
+  const { userInfo } = useUserInfoContext();
+  const { setPrivateChats } = usePrivateChatsContext();
+  const { setPublicChats } = usePublicChatsContext();
+
+  const privateChatChannelRef = useRef(null);
+  const publicChatChannelRef = useRef(null);
+
+  /* this is for updating chats sidebar when user has not changed their chat */
+  useEffect(() => {
+    if (userInfo) {
+      subscribeToPrivateChatsChannel(
+        privateChatChannelRef,
+        cable,
+        userInfo.id,
+        setPrivateChats,
+        updateChats
+      );
+      subscribeToPublicChatsChannel(
+        publicChatChannelRef,
+        cable,
+        setPublicChats,
+        updateChats
+      );
+    }
+    return () => {
+      unsubscribeToPrivateChatsChannel(privateChatChannelRef, cable);
+      unsubscribeToPublicChatsChannel(publicChatChannelRef, cable);
+    };
+  }, [userInfo]);
+
   return (
-    <UserInfoProvider>
-      <ActionCableProvider>
-        <VisibilityProvider>
-          <div className="h-[calc(100vh-100px)] grid grid-cols-1 grid-rows-[1fr,_auto] xs:grid-cols-[auto,_1fr] md:grid-cols-[auto,_1fr,_2fr] xs:grid-rows-1 gap-2">
-            <Menu />
-            <ChatSidebar />
-            <PeopleSidebar />
-            <ChatMain />
-          </div>
-        </VisibilityProvider>
-      </ActionCableProvider>
-    </UserInfoProvider>
+    <VisibilityProvider>
+      <div className="h-[calc(100vh-100px)] grid grid-cols-1 grid-rows-[1fr,_auto] xs:grid-cols-[auto,_1fr] md:grid-cols-[auto,_1fr,_2fr] xs:grid-rows-1 gap-2">
+        <Menu />
+        <ChatSidebar />
+        <PeopleSidebar />
+        <ChatMain />
+      </div>
+    </VisibilityProvider>
   );
 }
+
+// TODO: want to add PRIVATE AND PUBLIC CHATS subscriptions here, which means moving action cable provider
+// /* this is for getting rooms when user has added a new room -- DONT THINK I NEED */
+// useEffect(() => {
+//   const abortController = new AbortController();
+
+//   console.log("fetching chats");
+
+//   const url = isPrivate
+//     ? "/api/v1/private_chats/index"
+//     : "/api/v1/public_chats/index";
+
+//   getResource(url, abortController, setChats);
+
+//   return () => {
+//     abortController.abort();
+//   };
+// }, [userInfo]); // needed to re-fetch when new room is created?
