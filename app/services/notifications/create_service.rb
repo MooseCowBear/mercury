6b8f4c5d63ba82e599b0e2dc2357ebf09bc248cf
MooseCowBear <82609260+MooseCowBear@users.noreply.github.com>
2024-03-15 09:44:45 -0400

    add test for notifications service, remove notify method

--- app/services/notifications/create_service.rb
+++ app/services/notifications/create_service.rb
@@ -9,14 +9,8 @@ class Notifications::CreateService < ApplicationService
   end
 
   def call 
-    notify
-  end
-
-  private
-
-  def notify
     @message.notification_recipients.each do |recipient|
       recipient.notifications.create(user_id: recipient.id, message_id: @message.id, chat_id: @message.chat.id)
     end
-  end 
+  end
 end
---
+++ spec/services/notifications/create_service_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+RSpec.describe Notifications::CreateService, type: :service do
+  before(:each) do
+    @user = create(:user)
+    @user2 = create(:user)
+    @message = create(:message)
+  end
+
+  describe "self.call" do
+    it "creates a notification for each notification recipient of message" do
+      allow(@message).to receive(:notification_recipients).and_return([@user, @user2])
+      Notifications::CreateService.call(@message)
+      notifications = Notification.all
+      expect(notifications.length).to be 2
+      notification_for_user = Notification.find_by(user: @user)
+      expect(notification_for_user.chat.id).to eq @message.chat.id
+      expect(notification_for_user.message.id).to eq @message.id
+      notification_for_user2 = Notification.find_by(user: @user2)
+      expect(notification_for_user2.chat.id).to eq @message.chat.id
+      expect(notification_for_user2.message.id).to eq @message.id
+    end
+  end
+end
