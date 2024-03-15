8cdef5c807fcfb3eb4959394fb3cc05bb22ca41a
MooseCowBear <82609260+MooseCowBear@users.noreply.github.com>
2024-03-15 16:06:31 -0400

    remove unused files

--- app/javascript/frontend/utils/message.js
+++
@@ -1,9 +0,0 @@
-import copyObjectArr from "./copy"
-
-export const updateMessagesAfterDelete = (parsedResponse, messages) => {
-  const newMessages = copyObjectArr(messages);
-  const afterDelete = newMessages.filter(
-    (elem) => elem.id !== parsedResponse.id
-  );
-  return afterDelete;
-};
