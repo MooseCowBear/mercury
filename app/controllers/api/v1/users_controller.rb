dcf6e0cc3fd6ad711ac36773e2be7399689a6206
MooseCowBear <82609260+MooseCowBear@users.noreply.github.com>
2024-03-19 10:33:51 -0400

    use custom as json

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -3,7 +3,7 @@ class Api::V1::UsersController < ApplicationController
   before_action :set_chat, only: [:update]
   
   def show
-    render json: current_user, include: [:current_chat]
+    render json: current_user.to_json
   end
 
   def index
