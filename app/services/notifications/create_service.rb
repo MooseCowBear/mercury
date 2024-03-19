ad03c5eca7c44c23a939ae71fdbffc00c6d88ae1
MooseCowBear <82609260+MooseCowBear@users.noreply.github.com>
2024-03-19 10:02:24 -0400

    update test, fix typo

--- app/services/notifications/create_service.rb
+++ app/services/notifications/create_service.rb
@@ -1,16 +1,33 @@
 class Notifications::CreateService < ApplicationService
-  # notifications should be created for all the chat participants not currently
-  # in the chat when a new message for the chat is created. 
-  # this service handles the creation. the broadcast of the notification count 
-  # (the frontend only cares about the count not the actual notifications)
-  # gets included in the message broadcast service
+  # when a message is created, want two other sets of records to be created
+  # notifications for the messsage and private_message_recipient records
+  # notifications tell users who belong to a private chat but are outside it 
+  # at the time that a new message has been sent
+  # private_message_recipient records keep track of which chat members should receive
+  # a particular message, allowing for blocking and unblocking of chats to work
+  # as expected -- users who have a chat blocked will not receive a message sent during
+  # the time when the chat was blocked
+  
   def initialize(message)
     @message = message
   end
 
   def call 
+    create_notifications
+    create_message_recipients
+  end
+
+  private
+
+  def create_notifications
     @message.notification_recipients.each do |recipient|
       recipient.notifications.create(user_id: recipient.id, message_id: @message.id, chat_id: @message.chat.id)
+    end
+  end
+
+  def create_message_recipients
+    @message.message_recipients.each do |recipient|
+      recipient.private_message_recipients.create(user_id: recipient.id, message_id: @message.id)
     end
   end
 end
--- spec/services/notifications/create_service_spec.rb
+++ spec/services/notifications/create_service_spec.rb
@@ -10,6 +10,8 @@ RSpec.describe Notifications::CreateService, type: :service do
   describe "self.call" do
     it "creates a notification for each notification recipient of message" do
       allow(@message).to receive(:notification_recipients).and_return([@user, @user2])
+      allow(@message).to receive(:message_recipients).and_return([@user, @user2])
+
       Notifications::CreateService.call(@message)
       notifications = Notification.all
       expect(notifications.length).to be 2
@@ -19,6 +21,15 @@ RSpec.describe Notifications::CreateService, type: :service do
       notification_for_user2 = Notification.find_by(user: @user2)
       expect(notification_for_user2.chat.id).to eq @message.chat.id
       expect(notification_for_user2.message.id).to eq @message.id
+
+      message_recipients = PrivateMessageRecipient.all 
+      expect(message_recipients.length).to be 2
+      message_recipient_for_user = PrivateMessageRecipient.find_by(user: @user)
+      expect(message_recipient_for_user.message.id).to eq @message.id
+      message_recipient_for_user2 = PrivateMessageRecipient.find_by(user: @user2)
+      expect(message_recipient_for_user2.message.id).to eq @message.id
     end
   end
 end
+
+# update after rename
