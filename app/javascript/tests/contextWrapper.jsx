4cc2bd2ee2023d19071b2f71d7cf4e70c3ff2733
MooseCowBear <82609260+MooseCowBear@users.noreply.github.com>
2024-03-22 09:40:17 -0400

    add comment

--- app/javascript/tests/contextWrapper.jsx
+++ app/javascript/tests/contextWrapper.jsx
@@ -1,11 +1,13 @@
 import { render } from "@testing-library/react";
 import React from "react";
-
 import { PrivateChatsContext } from "../frontend/contexts/PrivateChatsContext";
 import { PublicChatsContext } from "../frontend/contexts/PublicChatsContext";
 import { UserInfoContext } from "../frontend/contexts/UserInfoContext";
 import { VisibilityContext } from "../frontend/contexts/VisibilityContext";
 import { ActionCableContext } from "../frontend/contexts/ActionCableContext";
+
+/* helper function to allow rendering a component with multiple contexts for tests. 
+renders component with as many context providers as needed wrapping it */
 
 const CONTEXT_MAP = {
   PrivateChatsContext,
