93156640f6201e7766b8d928f17a007840d13bfc
MooseCowBear <82609260+MooseCowBear@users.noreply.github.com>
2024-03-26 07:07:57 -0400

    add tests for message destroy

--- spec/requests/api/v1/messages_spec.rb
+++ spec/requests/api/v1/messages_spec.rb
@@ -44,6 +44,29 @@ RSpec.describe "Api::V1::Messages", type: :request do
   end
 
   describe "DELETE /api/v1/messages/" do
-    # finish once figure out behavior
+    before(:each) do
+      @private_chat = create(:chat, :private)
+    end
+
+    it "destroys the PrivateMessageParticipant record associated with current user and message if found" do
+      message = create(:message, user: @user, chat: @private_chat)
+      recipient_record = create(:private_message_recipient, user: @user, message: message)
+
+      expect { 
+        delete api_v1_message_path(message)
+      }.to change { PrivateMessageRecipient.count }.by(-1)
+
+      expect(response).to have_http_status(200)
+    end
+
+    it "responds with unprocessible entity if record can't be destroyed" do
+      message = create(:message, user: @user, chat: @private_chat)
+
+      expect { 
+        delete api_v1_message_path(message)
+      }.to change { PrivateMessageRecipient.count }.by(0)
+
+      expect(response).to have_http_status(422)
+    end
   end
 end
