bfe99980b4b694adaa161603353d937fb5f85da7
MooseCowBear <82609260+MooseCowBear@users.noreply.github.com>
2024-03-27 06:27:29 -0400

    remove unused channel

--- app/channels/users_channel.rb
+++
@@ -1,8 +0,0 @@
-class UsersChannel < ApplicationCable::Channel
-  def subscribed
-    stream_from "user_#{params[:user_id]}"
-  end
-
-  def unsubscribed
-  end
-end
--- app/javascript/channels/index.js
+++ app/javascript/channels/index.js
@@ -1,5 +1,4 @@
 // Import all the channels to be used by Action Cable
-import "./users_channel"
 import "./chat_channel"
 import "./private_chats_channel"
 import "./public_chats_channel"
