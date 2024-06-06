1f43f2e540063ee3a179807fe6790b5c67aa8526
MooseCowBear <82609260+MooseCowBear@users.noreply.github.com>
2024-06-06 12:50:25 -0400

    remove notification scopes that didn't do anything

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -2,7 +2,4 @@ class Notification < ApplicationRecord
   belongs_to :user
   belongs_to :chat
   belongs_to :message
-
-  scope :for_user, ->(user) { where(user: user) }
-  scope :for_chat, ->(chat) { where(chat: chat) }
 end
