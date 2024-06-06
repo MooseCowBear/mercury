4588e0c31e77a0dd032439b1426112b3df4d6d93
MooseCowBear <82609260+MooseCowBear@users.noreply.github.com>
2024-06-06 13:05:01 -0400

    remove unused before action

--- app/controllers/api/v1/private_chats_controller.rb
+++ app/controllers/api/v1/private_chats_controller.rb
@@ -1,6 +1,5 @@
 class Api::V1::PrivateChatsController < ApplicationController
   after_action -> { current_user.update_last_active if current_user }
-  before_action :set_chat, only: [:destroy]
   
   def index 
     chats = current_user.chats.has_message.order(updated_at: :desc) # these are the chats had through chat participants
@@ -20,10 +19,6 @@ class Api::V1::PrivateChatsController < ApplicationController
   end
 
   private
-
-  def set_chat
-    @chat = current_user.chats.find(params[:id])
-  end
 
   def private_chat_params
     params.require(:private_chat).permit(:name, chat_participants_attributes: [:user_id])
